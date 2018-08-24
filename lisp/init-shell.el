216c7e9e991ff12fbc3268444ee64cb6711c55bd
zsxh <bnbvbchen@gmail.com>
2018-08-25 00:50:41 +0800

    Add nxml/mvn-pom snippet and term-mode "paste" keybindings

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -40,6 +40,11 @@
   ;; The last line is needed or no picked up by 'shell-pop'
   (shell-pop--set-shell-type 'shell-pop-shell-type shell-pop-shell-type))
 
+(with-eval-after-load 'term
+  (evil-define-key 'normal term-raw-map "p" 'term-paste)
+  (evil-define-key 'insert term-raw-map "\C-y" 'term-paste))
+
+
 (provide 'init-shell)
 
 ;;; init-shell.el ends here
