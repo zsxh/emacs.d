8126c52f4b4c1c20054425d631a839a087d7ba91
zsxh <bnbvbchen@gmail.com>
2018-08-29 17:42:12 +0800

    Set gc-cons-threshold in emacs-startup-hook

--- init.el
+++ init.el
@@ -42,8 +42,8 @@
 (let ((normal-gc-cons-threshold (* 20 1024 1024))
       (init-gc-cons-threshold (* 128 1024 1024)))
   (setq gc-cons-threshold init-gc-cons-threshold)
-  (add-hook 'after-init-hook (lambda ()
-                               (setq gc-cons-threshold normal-gc-cons-threshold))))
+  (add-hook 'emacs-startup-hook (lambda ()
+                                  (setq gc-cons-threshold normal-gc-cons-threshold))))
 
 ;; Load Path
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
