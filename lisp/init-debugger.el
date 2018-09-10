608b9c22b163bab3c6f9307be4663b1e7efac693
zsxh <bnbvbchen@gmail.com>
2018-09-10 16:30:40 +0800

    Modify: remap debug-mode-map

--- lisp/init-debugger.el
+++ lisp/init-debugger.el
@@ -30,9 +30,21 @@
 
 ;;; Code:
 
+;; Debug/edebug evil keybindings
+(with-eval-after-load 'evil-collection
+  (with-eval-after-load 'debug
+    (evil-collection-init 'debug)
+    (evil-define-key 'normal debugger-mode-map (kbd "<return>") 'backtrace-help-follow-symbol))
+  (with-eval-after-load 'edebug
+    (evil-collection-init 'edebug)))
+
+;; Realgud Configs
 (use-package realgud
   :ensure t
-  :commands (realgud:gdb realgud:ipdb realgud:jdb))
+  :commands (realgud:gdb realgud:ipdb realgud:jdb)
+  :config
+  (with-eval-after-load 'evil-collection
+    (evil-collection-init 'realgud)))
 
 ;; dap-mode
 
