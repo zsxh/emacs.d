944a27f3ade24b7d8163c7ee3fd472cddef808a7
zsxh <bnbvbchen@gmail.com>
2018-09-26 15:50:47 +0800

    Update: customize lispy--eval-lisp function

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -182,7 +182,23 @@
               ("s-k" . paredit-splice-sexp-killing-backward))
   :config
   (require 'le-lisp)
-  (setq lispy-use-sly t))
+  (setq lispy-use-sly t)
+
+  ;; Replace lispy--eavl-lisp function
+  (defun lispy--eval-lisp-advice (str)
+    "Eval STR as Common Lisp code."
+    (let* ((deactivate-mark nil)
+           (result (with-current-buffer (process-buffer (lispy--cl-process))
+                     (if lispy-use-sly
+                         (sly-interactive-eval str)
+                       (slime-eval `(swank:eval-and-grab-output ,str))))))
+      (if (equal (car result) "")
+          (cadr result)
+        (concat (propertize (car result)
+                            'face 'font-lock-string-face)
+                "\n\n"
+                (cadr result)))))
+  (advice-add #'lispy--eval-lisp :override #'lispy--eval-lisp-advice))
 
 (use-package paredit
   :ensure t
