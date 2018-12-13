3adad5912cdab4110ab5b3b748d1391d50191955
zsxh <bnbvbchen@gmail.com>
2018-12-13 19:44:43 +0800

    add eglot/lsp-julia configs, but the server is useless now

--- lisp/init-julia.el
+++ lisp/init-julia.el
@@ -34,26 +34,68 @@
   :ensure t
   :defer t)
 
-;; TODO: julia-mode key bindings
 (use-package julia-repl
   :ensure t
   :commands julia-repl-mode
   :hook (julia-mode . julia-repl-mode))
 
-(require 'init-lsp)
+;; for eglot
 
-;; TODO: wait until lsp-juia server fully support version 1.0
-;; (use-package lsp-julia
-;;   :quelpa ((lsp-julia :fetcher github :repo "non-Jedi/lsp-julia"))
-;;   :requires init-lsp
-;;   :commands lsp-julia-enable
-;;   :preface
-;;   (defun +julia/lsp-julia-configs ()
-;;     (setq-local company-minimum-prefix-length 0)
-;;     (setq-local company-backends
-;;                 '((company-lsp :separate company-yasnippet)))
-;;     (lsp-julia-enable))
-;;   :hook (julia-mode . +julia/lsp-julia-configs))
+;; (require 'init-eglot)
+
+;; (with-eval-after-load 'eglot
+;;   (add-to-list 'eglot-server-programs
+;;                '(julia-mode . ("julia"
+;;                                "--startup-file=no"
+;;                                "--history-file=no"
+;;                                "-e"
+;;                                "using LanguageServer, Sockets, SymbolServer; server = LanguageServer.LanguageServerInstance(stdin, stdout, false, \"~./julia/enviroments/v1.0\", \"\", Dict()); run(server);"))))
+
+;; (add-hook 'julia-mode-hook 'eglot-ensure)
+
+
+;; for lsp
+
+;; (require 'lsp)
+;; (require 'lsp-clients)
+;; ;; (defcustom lsp-clients-julia-server-command
+;; ;;   "julia --startup-file=no --history-file=no -e \"using LanguageServer, Sockets, SymbolServer; server = LanguageServer.LanguageServerInstance(stdin, stdout, false, \\\"~/.julia/enviroments/v1.0\\\", \\\"\\\", Dict()); run(server);\""
+;; ;;   "The julia language server executable to use."
+;; ;;   :group 'lsp-julia
+;; ;;   :type 'string)
+
+;; (defcustom lsp-clients-julia-executable "julia"
+;;   "The julia executable to use.
+;; Leave as just the executable name to use the default behavior of
+;; finding the executable with `exec-path'."
+;;   :group 'lsp-julia
+;;   :risky t
+;;   :type 'file)
+
+;; (defcustom lsp-clients-julia-args '()
+;;   "Extra arguments for the julia executable."
+;;   :group 'lsp-julia
+;;   :risky t
+;;   :type '(repeat string))
+
+;; (setq lsp-clients-julia-args '("--startup-file=no"
+;;                                "--history-file=no"
+;;                                "-e"
+;;                                "using LanguageServer, Sockets, SymbolServer; server = LanguageServer.LanguageServerInstance(stdin, stdout, false, \"~./julia/enviroments/v1.0\", \"\", Dict()); run(server);"))
+
+;; (defun lsp-clients--julia-command ()
+;;   "Generate the language server startup command."
+;;   `(,lsp-clients-julia-executable ,@lsp-clients-julia-args))
+
+;; (lsp-register-client
+;;  (make-lsp-client
+;;   :new-connection (lsp-stdio-connection 'lsp-clients--julia-command)
+;;   :major-modes '(julia-mode)
+;;   :server-id 'julia))
+
+;; (add-hook 'julia-mode-hook (lambda ()
+;;                              (lsp)
+;;                              (setq-local company-backends '(company-capf))))
 
 
 (provide 'init-julia)
