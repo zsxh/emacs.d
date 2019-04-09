d65dbda9f1920ab33ed282a97b3d722aba9cf41f
zsxh <bnbvbchen@gmail.com>
2019-04-09 13:51:15 +0800

    Evil set vterm-mode initial state 'insert

--- lisp/init-shell-term.el
+++ lisp/init-shell-term.el
@@ -58,6 +58,8 @@
   :commands (vterm vterm-other-window)
   :preface (setq vterm-install t)
   :config
+  (with-eval-after-load 'evil
+    (evil-set-initial-state 'vterm-mode 'insert))
   (with-eval-after-load 'doom-themes
     (setf (elt ansi-color-names-vector 0) (doom-color 'bg)))
   (defun vterm-kill-buffer-after-exit (buf)
