4443cf3a6680c6561f917d760212607ed182cf71
zsxh <bnbvbchen@gmail.com>
2019-06-10 09:37:24 +0800

    Lazy load python config

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -55,86 +55,85 @@
 
   (add-hook 'python-mode-hook 'lsp))
 
-(defun +python/set-leader-keys ()
-  (+language-server/set-common-leader-keys python-mode-map)
-  (+funcs/set-leader-keys-for-major-mode
-   python-mode-map
-   "'"  '(+python/repl :which-key "repl")
-   "c"  '(nil :which-key "compile-exec")
-   "cc" '(+python/python-execute-file :which-key "execute-file")
-   "cC" '(+python/python-execute-file-focus :which-key "execute-file-focus")))
-
-(defun +python/pyenv-executable-find (command)
-  (executable-find command))
-
-(defun +python/python-setup-shell (&rest args)
-  (if (+python/pyenv-executable-find "ipython")
-      (progn (setq python-shell-interpreter "ipython")
-             (if (version< (replace-regexp-in-string "[\r\n|\n]$" "" (shell-command-to-string "ipython --version")) "5")
-                 (setq python-shell-interpreter-args "-i")
-               (setq python-shell-interpreter-args "--simple-prompt -i")))
-    (progn
-      (setq python-shell-interpreter-args "-i")
-      (setq python-shell-interpreter "python"))))
-
-(defun +python/python-toggle-breakpoint ()
-  "Add a break point, highlight it."
-  (interactive)
-  (let ((trace (cond ((+python/pyenv-executable-find "trepan3k") "import trepan.api; trepan.api.debug()")
-                     ((+python/pyenv-executable-find "wdb") "import wdb; wdb.set_trace()")
-                     ((+python/pyenv-executable-find "ipdb") "import ipdb; ipdb.set_trace()")
-                     ((+python/pyenv-executable-find "pudb") "import pudb; pudb.set_trace()")
-                     ((+python/pyenv-executable-find "ipdb3") "import ipdb; ipdb.set_trace()")
-                     ((+python/pyenv-executable-find "pudb3") "import pudb; pudb.set_trace()")
-                     (t "import pdb; pdb.set_trace()")))
-        (line (thing-at-point 'line)))
-    (if (and line (string-match trace line))
-        (kill-whole-line)
-      (progn
-        (back-to-indentation)
-        (insert trace)
-        (insert "\n")
-        (python-indent-line)))))
-
-;;;###autoload
-(defun +python/repl ()
-  "Open the Python REPL."
-  (interactive)
-  ;; (process-buffer (run-python nil t t))
-  (pop-to-buffer (process-buffer (python-shell-get-or-create-process)))
-  (evil-insert-state))
-
-(defun +python/python-execute-file (arg)
-  "Execute a python script in a shell."
-  (interactive "P")
-  ;; set compile command to buffer-file-name
-  ;; universal argument put compile buffer in comint mode
-  (let ((universal-argument t)
-        (compile-command (format "%s %s"
-                                 (+python/pyenv-executable-find python-shell-interpreter)
-                                 (file-name-nondirectory buffer-file-name))))
-    (if arg
-        (call-interactively 'compile)
-      (compile compile-command t)
-      (with-current-buffer (get-buffer "*compilation*")
-        ;; python-shell--interpreter default value is void...
-        ;; Fix inferior-python-mode initialization error
-        (setq python-shell--interpreter nil)
-        (setq python-shell--interpreter-args nil)
-        (inferior-python-mode)))))
-
-(defun +python/python-execute-file-focus (arg)
-  "Execute a python script in a shell and switch to the shell buffer in
- 'normal state'."
-  (interactive "P")
-  (+python/python-execute-file arg)
-  (switch-to-buffer-other-window "*compilation*")
-  (end-of-buffer)
-  (evil-normal-state))
-
-(setq python-indent-offset 2)
-
 (with-eval-after-load 'python
+  (defun +python/set-leader-keys ()
+    (+language-server/set-common-leader-keys python-mode-map)
+    (+funcs/set-leader-keys-for-major-mode
+     python-mode-map
+     "'" '(+python/repl :which-key "repl")
+     "c" '(nil :which-key "compile-exec")
+     "cc" '(+python/python-execute-file :which-key "execute-file")
+     "cC" '(+python/python-execute-file-focus :which-key "execute-file-focus")))
+
+  (defun +python/pyenv-executable-find (command)
+    (executable-find command))
+
+  (defun +python/python-setup-shell (&rest args)
+    (if (+python/pyenv-executable-find "ipython")
+        (progn (setq python-shell-interpreter "ipython")
+               (if (version< (replace-regexp-in-string "[\r\n|\n]$" "" (shell-command-to-string "ipython --version")) "5")
+                   (setq python-shell-interpreter-args "-i")
+                 (setq python-shell-interpreter-args "--simple-prompt -i")))
+      (progn
+        (setq python-shell-interpreter-args "-i")
+        (setq python-shell-interpreter "python"))))
+
+  (defun +python/python-toggle-breakpoint ()
+    "Add a break point, highlight it."
+    (interactive)
+    (let ((trace (cond ((+python/pyenv-executable-find "trepan3k") "import trepan.api; trepan.api.debug()")
+                       ((+python/pyenv-executable-find "wdb") "import wdb; wdb.set_trace()")
+                       ((+python/pyenv-executable-find "ipdb") "import ipdb; ipdb.set_trace()")
+                       ((+python/pyenv-executable-find "pudb") "import pudb; pudb.set_trace()")
+                       ((+python/pyenv-executable-find "ipdb3") "import ipdb; ipdb.set_trace()")
+                       ((+python/pyenv-executable-find "pudb3") "import pudb; pudb.set_trace()")
+                       (t "import pdb; pdb.set_trace()")))
+          (line (thing-at-point 'line)))
+      (if (and line (string-match trace line))
+          (kill-whole-line)
+        (progn
+          (back-to-indentation)
+          (insert trace)
+          (insert "\n")
+          (python-indent-line)))))
+
+  (defun +python/repl ()
+    "Open the Python REPL."
+    (interactive)
+    ;; (process-buffer (run-python nil t t))
+    (pop-to-buffer (process-buffer (python-shell-get-or-create-process)))
+    (evil-insert-state))
+
+  (defun +python/python-execute-file (arg)
+    "Execute a python script in a shell."
+    (interactive "P")
+    ;; set compile command to buffer-file-name
+    ;; universal argument put compile buffer in comint mode
+    (let ((universal-argument t)
+          (compile-command (format "%s %s"
+                                   (+python/pyenv-executable-find python-shell-interpreter)
+                                   (file-name-nondirectory buffer-file-name))))
+      (if arg
+          (call-interactively 'compile)
+        (compile compile-command t)
+        (with-current-buffer (get-buffer "*compilation*")
+          ;; python-shell--interpreter default value is void...
+          ;; Fix inferior-python-mode initialization error
+          (setq python-shell--interpreter nil)
+          (setq python-shell--interpreter-args nil)
+          (inferior-python-mode)))))
+
+  (defun +python/python-execute-file-focus (arg)
+    "Execute a python script in a shell and switch to the shell buffer in
+ 'normal state'."
+    (interactive "P")
+    (+python/python-execute-file arg)
+    (switch-to-buffer-other-window "*compilation*")
+    (end-of-buffer)
+    (evil-normal-state))
+
+  (setq python-indent-offset 2)
+
   (+python/python-setup-shell)
   (+python/set-leader-keys))
 
