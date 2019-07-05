a50d20d1b7350ea73201167b5f1a4e436af301c9
zsxh <bnbvbchen@gmail.com>
2019-07-05 09:38:32 +0800

    Update vterm version, remove multi-term

--- lisp/init-shell-term.el
+++ lisp/init-shell-term.el
@@ -20,8 +20,6 @@
   (setq shell-pop-term-shell personal-shell-executable)
   (cond ((functionp 'vterm)
          (setq shell-pop-shell-type '("vterm" "*vterm*" (lambda () (vterm)))))
-        ((functionp 'multi-term)
-         (setq shell-pop-shell-type '("multi-term" "*multi-term*" (lambda () (multi-term)))))
         (t (setq shell-pop-shell-type '("ansi-term" "*ansi-term*" (lambda () (ansi-term shell-pop-term-shell))))))
   (setq shell-pop-window-position "bottom")
   ;; The last line is needed or no picked up by 'shell-pop'
@@ -35,40 +33,23 @@
 
 ;; HIGHLY RECOMMENDED
 ;; https://github.com/akermu/emacs-libvterm
-;; On ArchLinux or Manjaro:
+;; On ArchLinux or Manjaro install libvterm first
 ;; sudo pacman -S libvterm
-;;
-;; Compile and Build emacs-libvterm module
-;; it take times because cmake will automatically download some requied libraries from the internet.
-;; M-x vterm-module-compile
-;; or
-;; You can compile and install the module yourself.
-;; cd ~/.emacs.d/elpa/vterm-<version>
-;; mkdir -p build
-;; cd build
-;; cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-;; make
 (use-package vterm
   ;; :quelpa (vterm :fetcher github :repo "akermu/emacs-libvterm"
   ;;                :files (:defaults "*.c" "*.h" "CMakeLists.txt"))
   :ensure t
-  :init (setq vterm-install t)
   :if (and (executable-find "vterm-ctrl")
            (executable-find "make")
            (executable-find "cmake")
            (fboundp 'module-load))
   :commands (vterm vterm-other-window)
-  :bind (:map vterm-mode-map
-              ("M-u" . ace-window))
+  :bind (:map vterm-mode-map ("M-u" . ace-window))
   :config
   (with-eval-after-load 'evil
     (evil-set-initial-state 'vterm-mode 'insert))
   (with-eval-after-load 'doom-themes
-    (setf (elt ansi-color-names-vector 0) (doom-color 'bg)))
-  (defun vterm-kill-buffer-after-exit (buf)
-    (when (buffer-live-p buf)
-      (kill-buffer buf)))
-  (add-hook 'vterm-exit-functions 'vterm-kill-buffer-after-exit))
+    (setf (elt ansi-color-names-vector 0) (doom-color 'bg))))
 
 (use-package term
   :ensure nil
@@ -90,13 +71,6 @@
   (with-eval-after-load 'evil
     (evil-define-key 'normal term-raw-map "p" 'term-paste)
     (evil-define-key 'insert term-raw-map "\C-y" 'term-paste)))
-
-;; https://www.emacswiki.org/emacs/MultiTerm
-(use-package multi-term
-  :quelpa ((multi-term :fetcher github :repo "manateelazycat/multi-term"))
-  :commands multi-term
-  :config
-  (setq multi-term-program personal-shell-executable))
 
 ;; Better eshell
 ;; https://github.com/manateelazycat/aweshell
