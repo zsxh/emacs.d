f992864a141c136b31e7bf21f740bd56820f2990
zsxh <bnbvbchen@gmail.com>
2019-09-12 10:35:14 +0800

    do not evaluate again during export

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -19,8 +19,9 @@
               ("C-M-<return>" . org-table-insert-hline))
   :commands org-open-at-point
   :config
-  (setq org-confirm-babel-evaluate nil) ;don't prompt me to confirm everytime I want to evaluate a block
+  (setq org-confirm-babel-evaluate nil) ; do not prompt me to confirm everytime I want to evaluate a block
   (setq org-time-stamp-formats '("<%Y-%m-%d %A>" . "<%Y-%m-%d %A %H:%M>"))
+  (setq org-export-use-babel nil)       ; do not evaluate again during export.
 
   ;; display/update images in the buffer after I evaluate
   (add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
