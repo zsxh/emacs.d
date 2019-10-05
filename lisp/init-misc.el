27f71d99f0c152b8cebff7700c5478c36768b25d
zsxh <bnbvbchen@gmail.com>
2019-10-05 16:40:51 +0800

    add `counsel-network-manager' to connect wifi

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -131,6 +131,28 @@
              powerthesaurus-lookup-word-at-point
              powerthesaurus-lookup-word-dwim))
 
+;; Use nmcli to manage network
+(defvar counsel-network-manager-history nil
+  "Network manager history.")
+
+(defun counsel-network-manager (&optional initial-input)
+  "Connect to wifi network."
+  (interactive)
+  (shell-command "nmcli device wifi rescan")
+  (let ((networks-list (s-split "\n" (shell-command-to-string "nmcli device wifi"))))
+    (ivy-read "Select network" networks-list
+              :initial-input initial-input
+              :require-match t
+              :history counsel-network-manager-history
+              :sort nil
+              :caller 'counsel-network-manager
+              :action (lambda (line)
+                        (let ((network (car (s-split " " (s-trim (s-chop-prefix "*" line)) t))))
+                          (message "Connecting to \"%s\".." network)
+                          (async-shell-command
+                           (format "nmcli device wifi connect %s"
+                                   (shell-quote-argument network))))))))
+
 
 (provide 'init-misc)
 
