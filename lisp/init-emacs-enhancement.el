29b8486aa912c26b2b95a7eea413c419ca9ffbb8
zsxh <bnbvbchen@gmail.com>
2019-10-12 17:15:11 +0800

    add a function toggle "j/k" next-line/scroll-line

--- lisp/init-emacs-enhancement.el
+++ lisp/init-emacs-enhancement.el
@@ -308,9 +308,25 @@ Hack to use `insert-sliced-image' to avoid jerky image scrolling."
   (define-key eww-mode-map (kbd "T") '+eww/browse-at-point-with-eaf)
   (define-key eww-mode-map (kbd "&") '+eww/browse-at-point-with-external-browser)
 
+  (setq +eww/scroll-line-jk t)
+
+  (defun +eww/toggle-scroll-line-shortcut ()
+    (interactive)
+    (if +eww/scroll-line-jk
+        (progn
+          (evil-define-key 'normal eww-mode-map
+            "j" 'evil-next-line
+            "k" 'evil-previous-line)
+          (setq +eww/scroll-line-jk nil))
+      (evil-define-key 'normal eww-mode-map
+        "j" 'evil-scroll-line-down
+        "k" 'evil-scroll-line-up)
+      (setq +eww/scroll-line-jk t)))
+
   (with-eval-after-load 'evil
     (with-eval-after-load 'evil-collection
       (evil-collection-init 'eww))
+
     (evil-define-key 'normal eww-mode-map
       "b" 'evil-backward-word-begin
       "w" 'evil-forward-word-begin
@@ -319,6 +335,7 @@ Hack to use `insert-sliced-image' to avoid jerky image scrolling."
       "G" 'evil-goto-line
       "h" 'evil-backward-char
       "l" 'evil-forward-char
+      "t" '+eww/toggle-scroll-line-shortcut
       "v" 'evil-visual-char
       "0" 'evil-digit-argument-or-evil-beginning-of-line
       "&" '+eww/browse-at-point-with-external-browser)
