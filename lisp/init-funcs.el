25bd228238e99474776350ccb7a79b6895bac547
zsxh <bnbvbchen@gmail.com>
2019-10-13 17:32:39 +0800

    add xah-shrink-whitespaces function

--- lisp/init-funcs.el
+++ lisp/init-funcs.el
@@ -90,12 +90,25 @@ If point was already at that position, move point to beginning of line."
                          " | sudo -S " command)))
 
 (defun +funcs/shrink-whitespaces ()
-  "Remove whitespaces around cursor to none."
+  "Remove whitespaces around cursor to just one, or none.
+
+Shrink any neighboring space tab newline characters to 1 or none.
+If cursor neighbor has space/tab, toggle between 1 or 0 space.
+If cursor neighbor are newline, shrink them to just 1.
+If already has just 1 whitespace, delete it.
+
+URL `http://ergoemacs.org/emacs/emacs_shrink_whitespace.html'
+Version 2018-04-02T14:38:04-07:00"
   (interactive)
   (let* (
+         ($eol-count 0)
          ($p0 (point))
-         $p1                            ; whitespace begin
-         $p2                            ; whitespace end
+         $p1 ; whitespace begin
+         $p2 ; whitespace end
+         ($charBefore (char-before))
+         ($charAfter (char-after ))
+         ($space-neighbor-p (or (eq $charBefore 32) (eq $charBefore 9) (eq $charAfter 32) (eq $charAfter 9)))
+         $just-1-space-p
          )
     (skip-chars-backward " \n\t")
     (setq $p1 (point))
@@ -103,7 +116,36 @@ If point was already at that position, move point to beginning of line."
     (skip-chars-forward " \n\t")
     (setq $p2 (point))
     (goto-char $p1)
-    (delete-region $p1 $p2)))
+    (while (search-forward "\n" $p2 t )
+      (setq $eol-count (1+ $eol-count)))
+    (setq $just-1-space-p (eq (- $p2 $p1) 1))
+    (goto-char $p0)
+    (cond
+     ((eq $eol-count 0)
+      (if $just-1-space-p
+          (delete-horizontal-space)
+        (progn (delete-horizontal-space)
+               (insert " "))))
+     ((eq $eol-count 1)
+      (if $space-neighbor-p
+          (delete-horizontal-space)
+        (progn (xah-delete-blank-lines) (insert " "))))
+     ((eq $eol-count 2)
+      (if $space-neighbor-p
+          (delete-horizontal-space)
+        (progn
+          (xah-delete-blank-lines)
+          (insert "\n"))))
+     ((> $eol-count 2)
+      (if $space-neighbor-p
+          (delete-horizontal-space)
+        (progn
+          (goto-char $p2)
+          (search-backward "\n" )
+          (delete-region $p1 (point))
+          (insert "\n"))))
+     (t (progn
+          (message "nothing done. logic error 40873. shouldn't reach here" ))))))
 
 (defun +funcs/narrow-or-widen-dwim ()
   "If the buffer is narrowed, it widens. Otherwise, it narrows to region, or Org subtree."
