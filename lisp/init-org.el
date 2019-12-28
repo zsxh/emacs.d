4d6b316a4b69cb6308cf914911037f3e8469507c
zsxh <bnbvbchen@gmail.com>
2019-12-28 13:43:03 +0800

    support latex, org-mode latex/pdf export

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -200,6 +200,42 @@
   :ensure t
   :commands org-tree-slide-mode)
 
+;; require TeX Live installation
+;; pacman -S texlive-bin texlive-core texlive-latexextra texlive-langchinese
+(use-package tex
+  :ensure auctex
+  :defer t
+  :custom
+  (TeX-auto-save t)
+  (TeX-parse-self t)
+  (TeX-master nil)
+  ;; to use pdfview with auctex
+  ;; (TeX-view-program-selection '((output-pdf "pdf-tools"))
+  ;;                             TeX-source-correlate-start-server t)
+  ;; (TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view")))
+  (TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
+  :hook
+  (LaTeX-mode . (lambda ()
+                  (turn-on-reftex)
+                  (setq reftex-plug-into-AUCTeX t)
+                  (reftex-isearch-minor-mode)
+                  (setq TeX-PDF-mode t)
+                  (setq TeX-source-correlate-method 'synctex)
+                  (setq TeX-source-correlate-start-server t)))
+  :config
+  (when (version< emacs-version "26")
+    (add-hook LaTeX-mode-hook #'display-line-numbers-mode)))
+
+(use-package org-edit-latex
+  :defer t
+  :after org)
+
+(use-package org2ctex
+  :ensure t
+  :after org
+  :config
+  (org2ctex-mode))
+
 
 (provide 'init-org)
 
