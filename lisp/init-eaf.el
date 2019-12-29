ec66dd689bde50d5f0374b85e72185c9bce976b9
zsxh <bnbvbchen@gmail.com>
2019-12-29 18:16:03 +0800

    update eaf

--- lisp/init-eaf.el
+++ lisp/init-eaf.el
@@ -17,10 +17,6 @@
 (use-package eaf
   :load-path "~/.emacs.d/submodules/emacs-application-framework"
   :commands (eaf-open eaf-open-url eaf-open-dash)
-  :preface
-  (defun eaf-qutebrowser ()
-    (interactive)
-    (eaf-open "eaf-qutebrowser"))
   :config
   (require 'dash)
   (when personal-eaf-grip-token
@@ -46,8 +42,9 @@
                             (58 . evil-ex)
                             (26 . evil-emacs-state)))))
 
-  (defun +eaf/switch-to-eww ()
-    (let* ((url buffer-url)
+  (defun eaf-switch-to-eww ()
+    (interactive)
+    (let* ((url eaf--buffer-url)
            (eww-buffer (car (-filter (lambda (buffer)
                                        (with-current-buffer buffer
                                          (and (derived-mode-p 'eww-mode)
@@ -57,130 +54,59 @@
           (switch-to-buffer eww-buffer)
         (eww url))))
 
-  (setq eaf-pdfviewer-keybinding
-        '(("j" . "scroll_up")
-          ("k" . "scroll_down")
-          ("d" . "scroll_up_page")
-          ("u" . "scroll_down_page")
-          ("t" . "switch_to_read_mode")
-          ("." . "scroll_to_home")
-          ("," . "scroll_to_end")
-          ("0" . "zoom_reset")
-          ("=" . "zoom_in")
-          ("-" . "zoom_out")
-          ("g" . "jump_to_page")
-          ("p" . "jump_to_percent")
-          ("[" . "remember_current_position")
-          ("]" . "remeber_jump")
-          ("i" . "toggle_inverted_mode")))
-
-  (setq eval-normal-state-eaf-browser-keybinding
-        '(("F" . "history_forward")
-          ("H" . "history_backward")
-          ("M-q" . "clean_all_cookie")
-          ("-" . "zoom_out")
-          ("=" . "zoom_in")
-          ("0" . "zoom_reset")
-          ("j" . "scroll_up")
-          ("k" . "scroll_down")
-          ("d" . "scroll_up_page")
-          ("u" . "scroll_down_page")
-          ("M-<" . "scroll_to_begin")
-          ("M->" . "scroll_to_bottom")))
-
   (defun eaf-open-dash (url &optional args)
     (interactive)
     (eaf-open url "browser" nil))
 
-  (defun eaf-monitor-key-event ()
-    (unless
-        (ignore-errors
-          (with-current-buffer (buffer-name)
-            (when (eq major-mode 'eaf-mode)
-              (let* ((event last-command-event)
-                     (key (make-vector 1 event))
-                     (key-command (format "%s" (key-binding key)))
-                     (key-desc (key-description key)))
+  (eaf-bind-key "scroll_up_page" "d" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key "scroll_down_page" "u" eaf-pdf-viewer-keybinding)
 
-                ;; Uncomment for debug.
-                ;; (message (format "!!!!! %s %s %s %s %s" event key key-command key-desc buffer-app-name))
+  ;; TODO: modify current local map
 
-                (cond
-                 ;; Fix #51 , don't handle F11 to make emacs toggle frame fullscreen status successfully.
-                 ((equal key-desc "<f11>")
-                  t)
-                 ((and (member buffer-app-name '("browser" "markdown-previewer"))
-                       (not (or (evil-emacs-state-p) (evil-insert-state-p))))
-                  (cond
-                   ((string-equal key-desc "h")
-                    (eaf-call "send_key" buffer-id "<left>"))
-                   ((string-equal key-desc "l")
-                    (eaf-call "send_key" buffer-id "<right>"))
-                   ((string-equal key-desc "T")
-                    (+eaf/switch-to-eww))
-                   (t
-                    (eaf-handle-app-key buffer-id key-desc eval-normal-state-eaf-browser-keybinding))))
-                 ;; Just send event when user insert single character.
-                 ;; Don't send event 'M' if user press Ctrl + M.
-                 ((and (or
-                        (equal key-command "self-insert-command")
-                        (equal key-command "completion-select-if-within-overlay"))
-                       (equal 1 (string-width (this-command-keys))))
-                  (cond ((equal buffer-app-name "pdf-viewer")
-                         (eaf-handle-app-key buffer-id key-desc eaf-pdfviewer-keybinding))
-                        ((equal buffer-app-name "video-player")
-                         (eaf-handle-app-key buffer-id key-desc eaf-videoplayer-keybinding))
-                        ((equal buffer-app-name "image-viewer")
-                         (eaf-handle-app-key buffer-id key-desc eaf-imageviewer-keybinding))
-                        ((equal buffer-app-name "camera")
-                         (eaf-handle-app-key buffer-id key-desc eaf-camera-keybinding))
-                        (t
-                         (eaf-call "send_key" buffer-id key-desc))))
-                 ((string-match "^[CMSs]-.*" key-desc)
-                  (cond ((equal buffer-app-name "browser")
-                         (let ((function-name-value (assoc key-desc eaf-browser-keybinding)))
-                           (if function-name-value
-                               (eaf-call "execute_function" buffer-id (cdr function-name-value))
-                             (let ((key-alias-value (assoc key-desc eaf-browser-key-alias)))
-                               (if key-alias-value
-                                   (eaf-call "send_key" buffer-id (cdr key-alias-value)))))))
-                        ((equal buffer-app-name "terminal")
-                         (let ((function-name-value (assoc key-desc eaf-terminal-keybinding)))
-                           (when function-name-value
-                             (eaf-call "execute_function" buffer-id (cdr function-name-value)))))))
-                 ((or
-                   (equal key-command "nil")
-                   (equal key-desc "RET")
-                   (equal key-desc "DEL")
-                   (equal key-desc "TAB")
-                   (equal key-desc "SPC")
-                   (equal key-desc "<backtab>")
-                   (equal key-desc "<home>")
-                   (equal key-desc "<end>")
-                   (equal key-desc "<left>")
-                   (equal key-desc "<right>")
-                   (equal key-desc "<up>")
-                   (equal key-desc "<down>")
-                   (equal key-desc "<prior>")
-                   (equal key-desc "<next>"))
-                  (eaf-call "send_key" buffer-id key-desc))
-                 (t
-                  (unless (or
-                           (equal key-command "keyboard-quit")
-                           (equal key-command "kill-this-buffer")
-                           (equal key-command "eaf-open"))
-                    (ignore-errors (call-interactively (key-binding key)))))))
-              ;; Set `last-command-event' with nil, emacs won't notify me buffer is ready-only,
-              ;; because i insert nothing in buffer.
-              (setq last-command-event nil))))
-      ;; If something wrong in `eaf-monitor-key-event', emacs will remove `eaf-monitor-key-event' from `pre-command-hook' hook list.
-      ;; Then we add `eaf-monitor-key-event' in `pre-command-hook' list again, hahahaha.
-      (run-with-timer
-       0.1
-       nil
-       (lambda ()
-         (progn
-           (add-hook 'pre-command-hook #'eaf-monitor-key-event)))))))
+  ;; (eaf-bind-key eaf-browser-toggle-keybinding "C-t" eaf-browser-keybinding)
+
+  ;; (setq eaf-browser-last-keybinding eaf-browser-keybinding)
+
+  ;; (setq eaf-browser-keybinding
+  ;;       '(("-" . "zoom_out")
+  ;;         ("=" . "zoom_in")
+  ;;         ("0" . "zoom_reset")
+  ;;         ("C-s" . "search_text_forward")
+  ;;         ("C-r" . "search_text_backward")
+  ;;         ("j" . "scroll_up")
+  ;;         ("k" . "scroll_down")
+  ;;         ("C-v" . "scroll_up_page")
+  ;;         ("C-y" . "yank_text")
+  ;;         ("C-w" . "kill_text")
+  ;;         ("f" . "open_link")
+  ;;         ("F" . "open_link_new_tab")
+  ;;         ("C-/" . "undo_action")
+  ;;         ("M-_" . "redo_action")
+  ;;         ("M-w" . "copy_text")
+  ;;         ("M-f" . "history_forward")
+  ;;         ("M-b" . "history_backward")
+  ;;         ("M-q" . "clean_all_cookie")
+  ;;         ("M-v" . "scroll_down_page")
+  ;;         ("M-<" . "scroll_to_begin")
+  ;;         ("M->" . "scroll_to_bottom")
+  ;;         ("<f5>" . "refresh_page")
+  ;;         ("T" . eaf-switch-to-eww)
+  ;;         ("C-t" . eaf-browser-toggle-keybinding)))
+
+  ;; (defun eaf-browser-toggle-keybinding ()
+  ;;   (interactive)
+  ;;   (let* ((temp eaf-browser-keybinding))
+  ;;     (setq eaf-browser-keybinding eaf-browser-last-keybinding
+  ;;           eaf-browser-last-keybinding temp)
+  ;;     (eaf--gen-keybinding-map (symbol-value 'eaf-browser-keybinding))
+  ;;     (print eaf-browser-keybinding)
+  ;;     (with-current-buffer (current-buffer)
+  ;;       (setq-local emulation-mode-map-alists
+  ;;                   (default-value 'emulation-mode-map-alists))
+  ;;       (push (list (cons t eaf-mode-map))
+  ;;             emulation-mode-map-alists)
+  ;;       (push 'evil-mode-map-alist emulation-mode-map-alists))))
+  )
 
 
 ;; Pdf viewer settings
