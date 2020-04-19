d5bf7e9c86f0551ab6823a6bfa832e16ced7aee3
zsxh <bnbvbchen@gmail.com>
2020-04-19 12:14:35 +0800

    don't show ccls-code-lens

--- lisp/init-lang-c.el
+++ lisp/init-lang-c.el
@@ -29,14 +29,14 @@
 
 (defun +c/setup ()
   (require 'ccls)
-  (add-hook 'lsp-after-initialize-hook
-            (lambda ()
-              (when (member major-mode '(c-mode c++-mode))
-                (ccls-code-lens-mode)
-                (make-variable-buffer-local 'hs-hide-hook)
-                (add-hook 'hs-hide-hook 'ccls-clear-code-lens)
-                (make-variable-buffer-local 'hs-show-hook)
-                (add-hook 'hs-show-hook 'ccls-request-code-lens))))
+  ;; (add-hook 'lsp-after-initialize-hook
+  ;;           (lambda ()
+  ;;             (when (member major-mode '(c-mode c++-mode))
+  ;;               (ccls-code-lens-mode)
+  ;;               (make-variable-buffer-local 'hs-hide-hook)
+  ;;               (add-hook 'hs-hide-hook 'ccls-clear-code-lens)
+  ;;               (make-variable-buffer-local 'hs-show-hook)
+  ;;               (add-hook 'hs-show-hook 'ccls-request-code-lens))))
   (let ((mode-map-symbol (intern (format "%s-map" major-mode)))
         (mode-hook (intern (format "%s-hook" major-mode))))
     (+language-server/set-common-leader-keys mode-map-symbol)
