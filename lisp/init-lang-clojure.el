d94dd2c9b86af9f57a2bf16423229a061aace635
zsxh <bnbvbchen@gmail.com>
2020-05-04 09:45:29 +0800

    don't enable lsp-mode for dtm/edn clojure files

--- lisp/init-lang-clojure.el
+++ lisp/init-lang-clojure.el
@@ -20,12 +20,16 @@
   :mode (("\\.\\(clj\\|dtm\\|edn\\)\\'" . clojure-mode)
          ("\\.cljc\\'" . clojurec-mode)
          ("\\.cljs\\'" . clojurescript-mode))
-  :hook ((clojure-mode clojurec-mode clojurescript-mode) . lsp-deferred)
+  :hook ((clojure-mode clojurec-mode clojurescript-mode) . +clojure/lsp)
   :config
   (require 'lsp-clojure)
-  (setq lsp-clojure-server-command  `("bash" "-c" ,(f-join lsp-server-install-dir "clojure-lsp/clojure-lsp")))
+  (setq lsp-clojure-server-command `("bash" "-c" ,(f-join lsp-server-install-dir "clojure-lsp/clojure-lsp")))
   (dolist (mode-map '(clojure-mode-map clojurec-mode-map clojurescript-mode-map))
-    (+language-server/set-common-leader-keys (symbol-value mode-map))))
+    (+language-server/set-common-leader-keys (symbol-value mode-map)))
+  (defun +clojure/lsp ()
+    (if (and buffer-file-name
+             (member (file-name-extension buffer-file-name) '("clj" "cljs" "cljc")))
+        (lsp-deferred))))
 
 ;; https://cider.mx/
 (use-package cider
