2319176ffef2659c53c5f39a9a6469bb7fb9e6fb
zsxh <bnbvbchen@gmail.com>
2020-07-21 23:17:52 +0800

    disable lispy-mode in cider-reple-mode

--- lisp/init-lang-emacs-lisp.el
+++ lisp/init-lang-emacs-lisp.el
@@ -69,8 +69,7 @@
           lisp-interaction-mode
           clojure-mode
           clojurec-mode
-          clojurescript-mode
-          cider-repl-mode) . lispy-mode)
+          clojurescript-mode) . lispy-mode)
   :config
   ;; this requires CIDER or cider--display-interactive-eval-result function
   (setq lispy-eval-display-style 'overlay)
