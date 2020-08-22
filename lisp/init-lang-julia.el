d34431b1c78680ad4b95a8e5aefcff6d65a2207f
zsxh <bnbvbchen@gmail.com>
2020-08-22 11:20:06 +0800

    update lsp-mode julia

--- lisp/init-lang-julia.el
+++ lisp/init-lang-julia.el
@@ -65,27 +65,9 @@
                    "\\)"))
         1 font-lock-type-face)))))
 
-;; TODO: port https://github.com/non-Jedi/eglot-jl to lsp julia
-;; SymbolServer.jl takes a very long time to process project dependencies
-;; https://github.com/julia-vscode/SymbolServer.jl/issues/56
-;; This is a one-time process that shouldn’t cause issues once the dependencies are cached,
-;; however it can take over a minute to process each dependency.
 (use-package lsp-julia
-  :defer t
-  :custom
-  (lsp-julia-package-dir nil)     ; use the globally installed version
-  (lsp-julia-default-environment "~/.julia/environments/v1.5")
-  :config
-  (defun +julia/lsp-get-root (dir)
-    "Get the (Julia) project root directory of the current file."
-    (expand-file-name (if dir (or (locate-dominating-file dir "JuliaProject.toml")
-                                  (locate-dominating-file dir "Project.toml")
-                                  lsp-julia-default-environment)
-                        lsp-julia-default-environment)))
-
-  (advice-add 'lsp-julia--get-root :override
-              (lambda ()
-                (+julia/lsp-get-root (buffer-file-name)))))
+  :load-path (lambda () (expand-file-name "submodules/lsp-julia" user-emacs-directory))
+  :defer t)
 
 (use-package julia-repl
   :commands julia-repl-mode)
