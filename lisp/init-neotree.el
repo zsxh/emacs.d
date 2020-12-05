83f47a25ee706eda939a2ebe3f2a90b5c5a84f92
zsxh <bnbvbchen@gmail.com>
2020-12-05 09:27:08 +0800

    [feat] add neotree collapse path support

--- lisp/init-neotree.el
+++ lisp/init-neotree.el
@@ -127,6 +127,104 @@
       (neotree-find (+project/root))
       (neotree-find origin-buffer-file-name))))
 
+;; HACK neotree
+;; https://github.com/jaypei/emacs-neotree/issues/147
+;; directory paths collapsed.
+(defun neo-get-collapse-node (node)
+  (condition-case nil
+      (let ((entries (cl-remove-if
+                      (lambda (file)
+                        (or
+                         (equal (file-name-nondirectory (directory-file-name file)) ".")
+                         (equal (file-name-nondirectory (directory-file-name file)) "..")))
+                      (directory-files node t))))
+        (if (and (= (length entries) 1)
+                 (file-directory-p (car entries)))
+            (neo-get-collapse-node (car entries))
+          node))
+    ('file-error
+     (message "Walk directory %S failed." node)
+     node)))
+
+(defun neo-buffer--insert-tree-a (path depth)
+  (if (eq depth 1)
+      (neo-buffer--insert-root-entry path))
+  (let* ((contents (neo-buffer--get-nodes path))
+         (nodes (car contents))
+         (leafs (cdr contents))
+         (default-directory path))
+    (dolist (node nodes)
+      (let* ((collapse-node (neo-get-collapse-node node))
+             (expanded (neo-buffer--expanded-node-p collapse-node))
+             (collapsed (> (length collapse-node) (length node)))
+             (short-name (if collapsed
+                             (substring collapse-node (1+ (length (neo-path--updir node))))
+                           nil))
+             (node (if collapsed collapse-node node)))
+        (neo-buffer--insert-dir-entry
+         node depth expanded short-name)
+        (if expanded (neo-buffer--insert-tree (concat node "/") (+ depth 1)))))
+    (dolist (leaf leafs)
+      (neo-buffer--insert-file-entry leaf depth))))
+
+(defun neo-buffer--insert-dir-entry-a (node depth expanded &optional short-name)
+  (let ((node-short-name (or short-name (neo-path--file-short-name node))))
+    (insert-char ?\s (* (- depth 1) 2)) ; indent
+    (when (memq 'char neo-vc-integration)
+      (insert-char ?\s 2))
+    (neo-buffer--insert-fold-symbol
+     (if expanded 'open 'close) node)
+    (insert-button (if neo-show-slash-for-folder (concat node-short-name "/") node-short-name)
+                   'follow-link t
+                   'face neo-dir-link-face
+                   'neo-full-path node
+                   'keymap neotree-dir-button-keymap
+                   'help-echo (neo-buffer--help-echo-message node-short-name))
+    (neo-buffer--node-list-set nil node)
+    (neo-buffer--newline-and-begin)))
+
+(advice-add #'neo-buffer--insert-tree :override #'neo-buffer--insert-tree-a)
+(advice-add #'neo-buffer--insert-dir-entry :override #'neo-buffer--insert-dir-entry-a)
+
+(with-eval-after-load '(and neotree doom-themes-ext-neotree)
+
+  (defun doom-themes-neotree-insert-dir-a (node depth expanded &optional short-name)
+    (let ((short-name (or short-name (neo-path--file-short-name node)))
+          (faces '(doom-themes-neotree-dir-face))
+          icon-text)
+      ;; insert indentation
+      (insert-char ?\s (* (- depth 1) 2))
+      ;; vcs integration
+      (let ((vc (if neo-vc-integration (neo-vc-for-node node))))
+        (when (memq 'char neo-vc-integration)
+          (insert-char (car vc))
+          (insert-char ?\s))
+        (unless (and (memq 'face neo-vc-integration)
+                     (not (eq (cdr vc) 'neo-vc-up-to-date-face))
+                     (setq faces (list (cdr vc))))
+          (cl-destructuring-bind (&key face icon)
+              (doom-themes-neotree-spec node doom-themes-neotree-dir-rules)
+            (if face (push face faces))
+            (if icon (setq icon-text icon)))))
+      ;; insert icon
+      (let ((type (if expanded 'open 'close)))
+        (if (display-graphic-p)
+            (doom--neotree-insert-icon type node icon-text faces)
+          (neo-buffer--insert-fold-symbol type node)))
+      ;; insert label button
+      (when doom-themes-neotree-enable-variable-pitch
+        (push 'variable-pitch faces))
+      (insert-button short-name
+                     'follow-link t
+                     'face `(:inherit (,@faces))
+                     'neo-full-path node
+                     'keymap neotree-dir-button-keymap)
+      ;; metadata + newline
+      (neo-buffer--node-list-set nil node)
+      (neo-buffer--newline-and-begin)))
+
+  (advice-add #'doom-themes-neotree-insert-dir :override #'doom-themes-neotree-insert-dir-a))
+
 
 (provide 'init-neotree)
 
