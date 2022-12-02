f54b8e594360b769c61ce38f6e6200564f632fad
zsxh <bnbvbchen@gmail.com>
2022-12-03 00:24:56 +0800

    clean up custom faces

--- site-lisp/zsxh-theme-custom.el
+++ site-lisp/zsxh-theme-custom.el
@@ -21,20 +21,7 @@
        (with-eval-after-load 'dired
          (set-face-foreground 'dired-directory "#3B6EA8"))
        (with-eval-after-load 'all-the-icons-dired
-         (set-face-foreground 'all-the-icons-dired-dir-face "#3B6EA8"))
-       (with-eval-after-load 'markdown-mode
-         (set-face-background 'markdown-code-face "#E0E0E0"))
-       (with-eval-after-load 'org
-         ;; Org block face
-         (set-face-background 'org-block "#E0E0E0")
-         (set-face-background 'org-quote nil)
-         (set-face-background 'org-block-begin-line nil)
-         (set-face-background 'org-block-end-line nil))
-       (with-eval-after-load 'mmm-vars
-         (set-face-background 'mmm-default-submode-face "#E5E5E5"))
-       (with-eval-after-load 'jupyter-repl
-         (set-face-foreground 'jupyter-repl-input-prompt "#4F894C")
-         (set-face-background 'jupyter-repl-traceback "#FBF8EF"))))
+         (set-face-foreground 'all-the-icons-dired-dir-face "#3B6EA8"))))
     ('doom-one-light
      (progn
        (with-eval-after-load 'dired
@@ -46,22 +33,7 @@
        (with-eval-after-load 'dired
          (set-face-foreground 'dired-directory "#51afef"))
        (with-eval-after-load 'all-the-icons-dired
-         (set-face-foreground 'all-the-icons-dired-dir-face "#3B6EA8"))
-       (with-eval-after-load 'org
-         (set-face-background 'org-quote nil)
-         (set-face-background 'org-block-begin-line nil)
-         (set-face-background 'org-block-end-line nil)
-         (set-face-attribute 'org-verbatim nil
-                             :foreground "#98be65"
-                             :background "#2e332d"
-                             :inherit 'fixed-pitch))
-       (with-eval-after-load 'ein-cell
-         (set-face-attribute 'ein:cell-input-area nil :background "#22262e")
-         (set-face-attribute 'ein:cell-input-prompt nil :foreground "#4F894C" :background "#282c34")
-         (set-face-attribute 'ein:cell-output-prompt nil :foreground "darkred" :background "#282c34"))
-       (with-eval-after-load 'jupyter-repl
-         (set-face-foreground 'jupyter-repl-input-prompt "#4F894C")
-         (set-face-background 'jupyter-repl-traceback "#4B483F"))))
+         (set-face-foreground 'all-the-icons-dired-dir-face "#3B6EA8"))))
     ('doom-solarized-light
      (progn
        (with-eval-after-load 'dired
@@ -79,27 +51,9 @@
        (with-eval-after-load 'paren
          (set-face-background 'show-paren-match "#4e4e4e")))))
 
-  ;; https://www.reddit.com/r/emacs/comments/diahh1/emacs_27_update_changed_how_highlighted_lines/
-  ;; The new face attribute ':extend' controls whether to use the face for displaying the empty space beyond end of line (EOL) till the edge of the window.
-  (when (fboundp 'set-face-extend)
-    (with-eval-after-load 'org
-      (set-face-extend 'org-block t)
-      (set-face-extend 'org-block-begin-line t)
-      (set-face-extend 'org-block-end-line t))
-    (with-eval-after-load 'ein-cell
-      (set-face-extend 'ein:cell-input-area t))
-    (with-eval-after-load 'jupyter-repl
-      (set-face-extend 'jupyter-repl-traceback t)))
-
   ;; global settings
   (with-eval-after-load 'magit-diff
-    (set-face-attribute 'magit-diff-revision-summary nil :inherit 'magit-diff-hunk-heading-highlight))
-
-  (with-eval-after-load 'diff-hl
-    (let ((bg (face-attribute 'default :background)))
-      (set-face-background 'diff-hl-insert bg)
-      (set-face-background 'diff-hl-delete bg)
-      (set-face-background 'diff-hl-change bg))))
+    (set-face-attribute 'magit-diff-revision-summary nil :inherit 'magit-diff-hunk-heading-highlight)))
 
 
 (provide 'zsxh-theme-custom)
