6e39bb499adc987abc300a6589f668151fec2dee
zsxh <bnbvbchen@gmail.com>
2022-12-29 17:05:10 +0800

    do not copy package-selected-packages to init/custom file

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -17,6 +17,15 @@
   (setq package-enable-at-startup nil) ; To prevent initializing twice
   (package-initialize))
 
+;; HACK: DO NOT copy package-selected-packages to init/custom file forcibly.
+;; https://github.com/jwiegley/use-package/issues/383#issuecomment-247801751
+(defun my-save-selected-packages (&optional value)
+  "Set `package-selected-packages' to VALUE but don't save to `custom-file'."
+  (when value
+    (setq package-selected-packages value)))
+
+(advice-add 'package--save-selected-packages :override #'my-save-selected-packages)
+
 ;; Set EPLA
 (defun set-package-archives (archives)
   "Set specific package ARCHIVES repository like 'melpa,
