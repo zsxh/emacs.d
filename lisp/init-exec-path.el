9d86374561f4c5708043729594530911ac4bd043
zsxh <bnbvbchen@gmail.com>
2023-05-22 14:34:39 +0800

    exec-path-from-shell copy env variables from interactive login shell

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -18,7 +18,7 @@
   (setq
    ;; NOTE: https://github.com/purcell/exec-path-from-shell#making-exec-path-from-shell-faster
    ;; load from .zshenv, no need to load .zshrc(interactive login shell)
-   exec-path-from-shell-arguments nil
+   ;; exec-path-from-shell-arguments nil
    exec-path-from-shell-variables '("PATH" "MANPATH" "LANG" "LANGUAGE" "LD_LIBRARY_PATH"))
 
   (defun exec-path-from-shell-initialize-async ()
