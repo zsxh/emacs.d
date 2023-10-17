4d76f948bb8c26eaf1e4f5c28d532a15caefa929
zsxh <bnbvbchen@gmail.com>
2023-10-17 09:43:01 +0800

    upstream emacs had fixed eglot completion bug

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -28,7 +28,6 @@
   (add-hook 'eglot-managed-mode-hook #'breadcrumb-local-mode))
 
 (with-eval-after-load 'eglot
-  (add-to-list 'completion-category-overrides '(eglot (styles basic substring)))
 
   (defvar +eglot/display-buf "*+eglot/display-buffer*")
   (defvar +eglot/display-frame nil)
