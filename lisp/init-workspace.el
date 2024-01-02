eb9f1dc7e3488e7ec792acd3100f06a7fed56ed1
zsxh <bnbvbchen@gmail.com>
2024-01-02 17:24:10 +0800

    remove tabspaces.el

--- lisp/init-workspace.el
+++ lisp/init-workspace.el
@@ -10,27 +10,12 @@
 
 ;;; Code:
 
-;; TODO: emacs built-in `tab-bar'
+;; emacs built-in `tab-bar'
 (use-package tab-bar
   :ensure nil
   :hook (after-init . tab-bar-mode)
   :custom
   (tab-bar-show 1))
-
-;; TODO: https://github.com/mclear-tools/tabspaces
-(use-package tabspaces
-  :defer t
-  ;; :hook (after-init . tabspaces-mode)
-  :custom
-  ;; (tab-bar-show nil)
-
-  (tabspaces-use-filtered-buffers-as-default t)
-  (tabspaces-default-tab "Default")
-  (tabspaces-remove-to-default t)
-  (tabspaces-include-buffers '("*scratch*"))
-  ;; sessions
-  (tabspaces-session t)
-  (tabspaces-session-auto-restore nil))
 
 
 (provide 'init-workspace)
