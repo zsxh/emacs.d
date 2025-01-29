594f568f801466557f3a912607e6dc0dbd9be1f3
zsxh <bnbvbchen@gmail.com>
2025-01-29 11:34:05 +0800

    feat(workspace): persist and restore visible buffers including tab-bar tabs

--- lisp/init-workspace.el
+++ lisp/init-workspace.el
@@ -47,6 +47,24 @@
 (use-package easysession
   :commands (easysession-save-as easysession-switch-to)
   :config
+  ;; Only persist and restore visible buffers
+  ;; https://github.com/jamescherti/easysession.el?tab=readme-ov-file#how-to-only-persist-and-restore-visible-buffers
+  (defun my-easysession-visible-buffer-list ()
+    "Return a list of all visible buffers in the current session.
+This includes buffers visible in windows or tab-bar tabs."
+    (let ((visible-buffers '()))
+      (dolist (buffer (buffer-list))
+        (when (or
+               ;; Windows
+               (get-buffer-window buffer 'visible)
+               ;; Tab-bar windows
+               (and (bound-and-true-p tab-bar-mode)
+                    (fboundp 'tab-bar-get-buffer-tab)
+                    (tab-bar-get-buffer-tab buffer t nil)))
+          (push buffer visible-buffers)))
+      visible-buffers))
+  (setq easysession-buffer-list-function #'my-easysession-visible-buffer-list)
+
   (when tab-bar-mode
     (advice-add 'easysession-switch-to :after
                 (lambda (&optional session-name)
