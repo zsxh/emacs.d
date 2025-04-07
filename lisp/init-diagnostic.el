8f39aaccf0837fd544769de2ce88723f3312cec1
zsxh <bnbvbchen@gmail.com>
2025-04-07 14:42:09 +0800

    chore(diagnostic): improve Flymake diagnostics update with Eglot integration
    Update Flymake settings to avoid interference with Corfu and ensure diagnostics refresh on LSP notifications when buffer is unmodified and no idle timeout is set.

--- lisp/init-diagnostic.el
+++ lisp/init-diagnostic.el
@@ -23,11 +23,23 @@
   ;; https://github.com/joaotavora/eglot/discussions/964
   ;; https://github.com/joaotavora/eglot/commit/2b87b06d9ef15e7c39d87fd5a4375b6deaa7e322
   ;; https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_pullDiagnostics
-  (setq flymake-no-changes-timeout 0.5
+  (setq flymake-no-changes-timeout nil
         flymake-start-on-save-buffer nil
         ;; FIXME: mess up corfu popup child frame, https://github.com/minad/corfu/issues/414
-        flymake-show-diagnostics-at-end-of-line nil)
-  (advice-add 'flymake-eldoc-function :override #'ignore))
+        flymake-show-diagnostics-at-end-of-line 'short)
+
+  (advice-add 'flymake-eldoc-function :override #'ignore)
+
+  ;; NOTE: https://github.com/joaotavora/eglot/issues/1296#issuecomment-1727978307
+  (with-eval-after-load 'eglot
+    (cl-defmethod eglot-handle-notification :after
+      (_server (_method (eql textDocument/publishDiagnostics)) &key uri
+               &allow-other-keys)
+      (when-let ((buffer (find-buffer-visiting (eglot-uri-to-path uri))))
+        (with-current-buffer buffer
+          (if (and (eq nil flymake-no-changes-timeout)
+                   (not (buffer-modified-p)))
+              (flymake-start t)))))))
 
 ;; https://github.com/mohkale/flymake-collection
 ;; TODO: flymake checkers for non-lsp files: yaml, xml, json...
